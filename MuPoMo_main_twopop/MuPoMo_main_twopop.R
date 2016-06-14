d81505c6665ae4c7ca4ef5c2018cc6b111341cf8
Lei Fang <lei.fang@outlook.de>
2016-06-14 02:19:14 +0200

    20160614-2
    updates on main_twopop

--- MuPoMo_main_twopop/MuPoMo_main_twopop.R
+++ MuPoMo_main_twopop/MuPoMo_main_twopop.R
@@ -1,72 +1,69 @@
-# ------------------------------------------------------------------------------ Project: Mortality Model for Multip-populations:
-# A Semiparametric Comparison Approach ------------------------------------------------------------------------------ Quantlet:
-# two-population.R ------------------------------------------------------------------------------ Description: Estimate and
-# forecast mortality rates based on a semi-parametric approach, which applies parametric modelling for multiple nonparametric
-# curves with the shape-related nonlinear variation.  Use China and Japan to demonstrate two-country case.
-# ------------------------------------------------------------------------------ Keywords: nonparametric smoothing, parametric
-# modeling, Lee-Carter method, mortality, China, Japan
-# ------------------------------------------------------------------------------ See also: multipop.R
-# ------------------------------------------------------------------------------ Author: Lei Fang
-# ------------------------------------------------------------------------------
+# clear history and close windows
+rm(list=ls(all=TRUE))
+graphics.off()
 
-setwd("/Users/lei.fang/Desktop/MuPoMo")
+# please set working directory
+# setwd("C:/...")     # windows
+# setwd("/Users/...") # mac os
+# setwd("~/...")      # linux
 
 # install packages
 library(demography)
-library(locpol)
 library(rgl)
 library(sm)
-library(boot)
 
-
+# set plot
 par(mar = c(5, 5, 2, 2), cex.axis = 1.5, cex.lab = 2)
 
-# read data and compute kt based on Lee-Carter Model source('data.R')
-load("~/Desktop/MuPoMo/data.RData")
-source("optimization-3.R")
+# recall self-defined functions
+# read data and compute kt based on Lee-Carter Model via source('data.R')
+source('MuPoMo_data.R')
+source("MuPoMo_optimization.R")
 
 # choose two countries' kt for this analysis
 kt1 = kt.China.female
 kt2 = kt.Japan.female
 
 # set bandwidth for smoothing curves
-bw.default = 2.5
+bw.default     = 2.5
 
+# plot kt
 plot(kt2, type = "l", col = "blue", lwd = 4, xlab = "Time", ylab = "Kt")
 lines(kt1, col = "red", lwd = 4)
 
 # shift movement of the two smoothed countries
-t1 = 1994:2010
-d1 = data.frame(time(kt1), kt1)
+t1         = 1994:2010 # differ based on the chosen country
+d1         = data.frame(time(kt1), kt1)
 h.optimal1 = h.select(time(kt1), kt1)
-sm1 = sm.regression(time(kt1), kt1, h = h.optimal1, eval.points = time(kt1), model = "none", poly.index = 1, display = "none")
-sm.kt1 = sm1$estimate  # smoothed kt1
+sm1        = sm.regression(time(kt1), kt1, h = h.optimal1, eval.points = time(kt1), model = "none", poly.index = 1, display = "none")
+sm.kt1     = sm1$estimate  # smoothed kt1
 
-t2 = 1947:2012
-d2 = data.frame(time(kt2), kt2)
+t2         = 1947:2012 # differ based on the chosen country
+d2         = data.frame(time(kt2), kt2)
 h.optimal2 = h.select(time(kt2), kt2)
-sm2 = sm.regression(time(kt2), kt2, h = h.optimal2, eval.points = time(kt2), model = "none", poly.index = 1, display = "none")
-sm.kt2 = sm2$estimate  # smoothed kt2
+sm2        = sm.regression(time(kt2), kt2, h = h.optimal2, eval.points = time(kt2), model = "none", poly.index = 1, display = "none")
+sm.kt2     = sm2$estimate  # smoothed kt2
 
 plot(t2, sm.kt2, type = "l", col = "blue", lwd = 4, xlab = "Time", ylab = "Kt")
 lines(kt2, col = "red", lwd = 4)
 lines(t1, sm.kt1, lwd = 4)
 lines(kt1, col = "darkgreen", lwd = 4)
-lines(t2 + 20, sm.kt2, lwd = 4, lty = 2)
+lines(t2 + 20, sm.kt2, lwd = 4, lty = 2) # differ based on the chosen country
 lines(t2 + 25, sm.kt2, lwd = 4, lty = 3)
 lines(t2 + 30, sm.kt2, lwd = 4, lty = 4)
 
 # time delay
 plot(t2, sm.kt2, type = "l", col = "blue", lwd = 4, xlab = "Time", ylab = "Kt")
 lines(t1, sm.kt1, lwd = 4)
-lines(t2 + 23, sm.kt2, lwd = 4, lty = 4, col = "blue")
+lines(t2 + 23, sm.kt2, lwd = 4, lty = 4, col = "blue") # differ based on the chosen country
+
 # vertical shift
 plot(t2, sm.kt2, type = "l", col = "blue", lwd = 4, xlab = "Time", ylab = "Kt")
 lines(t1, sm.kt1, lwd = 4)
-lines(t2, sm.kt2 + 85, lwd = 4, lty = 4, col = "blue")
+lines(t2, sm.kt2 + 85, lwd = 4, lty = 4, col = "blue") # differ based on the chosen country
 
 # optimization
-theta.2pop = c(1, -23, 1)
+theta.2pop  = c(1, -23, 1) # differ based on the chosen country
 result.2pop = optimization(theta.2pop, kt1, kt2)
 
 # plot optimized movement
@@ -103,10 +100,10 @@ loss = function(theta1, theta2, theta3, theta4, t, kt, t.reference, kt.reference
 }
 
 # plot loss function of theta2 and theta4
-vtheta2 = seq(-30, 10, len = 100)
-vtheta4 = seq(0, 100, len = 100)
-npar = length(vtheta2)
-res = matrix(rep(0, 10000), 100, 100)
+vtheta2 = seq(-30, 10, len = 100) # differ based on the chosen country
+vtheta4 = seq(0, 100, len = 100) # differ based on the chosen country
+npar    = length(vtheta2)
+res     = matrix(rep(0, 10000), 100, 100)
 for (i in 1:npar) {
     for (l in 1:npar) {
         res[i, l] = loss(1, vtheta2[i], 1, vtheta4[l], t1, kt1, t2, kt2)
@@ -125,18 +122,18 @@ contour(vtheta2, vtheta4, res, col = rainbow(20), nlevels = 70, lwd = 1, xlim = 
 contour(vtheta2, vtheta4, res, col = rainbow(20), nlevels = 70, lwd = 1, xlim = c(-10, -2), ylim = c(40, 80), zlim = c(0, 100))
 
 # plot loss function of theta2
-vtheta22 = seq(-50, 0, len = 100)
-npar2 = length(vtheta22)
-res2 = rep(0, 100)
+vtheta22 = seq(-50, 0, len = 100) # differ based on the chosen country
+npar2    = length(vtheta22)
+res2     = rep(0, 100)
 for (i in 1:npar2) {
     res2[i] = loss(1, vtheta22[i], 1, 0, t1, kt1, t2, kt2)
 }
 plot(vtheta22, res2, col = "blue", xlab = "theta2", ylab = "Loss", type = "l", lwd = 4)
 
 # plot loss function of theta4
-vtheta42 = seq(40, 140, len = 100)
-npar3 = length(vtheta42)
-res3 = rep(0, 100)
+vtheta42 = seq(40, 140, len = 100) # differ based on the chosen country
+npar3    = length(vtheta42)
+res3     = rep(0, 100)
 for (i in 1:npar3) {
     res3[i] = loss(1, 0, 1, vtheta42[i], t1, kt1, t2, kt2)
 }
